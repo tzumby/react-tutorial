7843a4c7218bfb166029fc69435aaffc6214dc1a
Razvan <tzumby@gmail.com>
2017-01-17 15:36:32 +0700

    Added production webpack config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,15 +1,19 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
   entry: './src/entry.jsx',
-  devtool: 'source-map',
+  devtool: 'cheap-module-source-map',
   output: {
     path: __dirname + '/build',
+    publicPath: __dirname + '/build/static',
     filename: 'bundle.js'
   },
   module: {
     loaders: [
       {
         test: /\.css$/,
-        loader: 'style!css'
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
       },
       {
         test: /\.jsx?$/,
@@ -27,10 +31,19 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      warnings: false,
+    }),
+    new ExtractTextPlugin("bundle.css")
+  ]
   resolve: {
     extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    port: 8181
-  },
+  }
 };
